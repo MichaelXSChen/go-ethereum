c86381606df1d7d9851cc52e5ec62fecd9e28d23
Michael Xusheng Chen <michael.xschen@gmail.com>
2018-03-21 16:20:00 +0800

    nit

--- consensus/trustedHW/trustedhw.go
+++ consensus/trustedHW/trustedhw.go
@@ -6,6 +6,15 @@ import (
 	"github.com/ethereum/go-ethereum/consensus"
 )
 
+var (
+	// errUnknownBlock is returned when the list of signers is requested for a block
+	// that is not part of the local blockchain.
+	errUnknownBlock = errors.New("unknown block")
+)
+
+
+
+
 type Config struct{
 	//parameters:
 
@@ -23,18 +32,55 @@ func (thw *TrustedHW) Author(header *types.Header) (common.Address, error){
 
 func (thw *TrustedHW) VerifyHeader (chain consensus.ChainReader, header *types.Header, seal bool) error {
 
+	return thw.verifyHeader(chain, header, nil);
+}
 
+// VerifyHeaders is similar to VerifyHeader, but verifies a batch of headers
+// concurrently. The method returns a quit channel to abort the operations and
+// a results channel to retrieve the async verifications.
+//XS: its an async function.
+func (thw *TrustedHW) VerifyHeaders(chain consensus.ChainReader, headers []*types.Header, seals []bool) (chan<- struct{}, <-chan error) {
+	abort := make(chan struct{})
+	results := make(chan error, len(headers))
 
+	go func() {
+		for i, header := range headers {
+			err := thw.verifyHeader(chain, header, headers[:i])
+
+			select {
+			case <-abort:
+				return
+			case results <- err:
+			}
+		}
+	}()
+	return abort, results
+}
+// verifyHeader checks whether a header conforms to the consensus rules.The
+// caller may optionally pass in a batch of parents (ascending order) to avoid
+// looking those up from the database. This is useful for concurrently verifying
+// a batch of new headers.
+func (thw *TrustedHW) verifyHeader(chain consensus.ChainReader, header *types.Header, parents []*types.Header) error {
 	//step 1: Sanity check.
+
+	if header.Number == nil {
+		return errUnknownBlock
+	}
 	number := header.Number.Uint64()
 	//already in the local chain
 	if chain.GetHeader(header.Hash(), number) != nil {
 		return nil
 	}
 	//same as ethash, check ancestor first
-	parent := chain.GetHeader(header.ParentHash, number-1)
-	if parent == nil {
-		return consensus.ErrUnknownAncestor
+	var parent *types.Header
+
+	if parents == nil || len(parents) == 0 {
+		parent = chain.GetHeader(header.ParentHash, number-1)
+		if parent == nil {
+			return consensus.ErrUnknownAncestor
+		}
+	}else{
+		parent = parents[0]
 	}
 	//Step 2: check author is in the committee list
 
@@ -43,5 +89,6 @@ func (thw *TrustedHW) VerifyHeader (chain consensus.ChainReader, header *types.H
 	//Step 4: check the author's signature.
 
 	return nil
-	
 }
+
+
